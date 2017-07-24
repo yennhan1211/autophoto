ba81da0c323d703cf2085972e939aa84c9e1df63
Anh Nguyen <yennhan1211@gmail.com>
2017-07-24 14:21:03 +0700

    Upadte upload code

--- upload.py
+++ upload.py
@@ -25,6 +25,7 @@ except ImportError:
 SCOPES = 'https://www.googleapis.com/auth/drive.file'
 CLIENT_SECRET_FILE = 'client_secrets.json'
 APPLICATION_NAME = 'Img upload'
+META_FILE        = "meta.txt"
 
 class Upload(threading.Thread):
     """docstring for Upload"""
@@ -33,14 +34,30 @@ class Upload(threading.Thread):
         self.name = name
         self.mQueue = uplQueue
         self.stoprequest = threading.Event()
+        self.defaultPath = os.getcwd()
+        self.service = None
 
     def run(self):
+        metaFilePath = os.path.join(self.defaultPath, META_FILE)
         while not self.stoprequest.isSet():
             try:
                 filePath = self.mQueue.get(True, 0.05)
                 logging.info('Uploading file at: ' + filePath)
+                retVal = self.insertFile(os.path.basename(filePath), "%Y%m%d-%H%M%S", None,'image/jpeg', filePath)
+                if retVal is None
+                    logging.info("Upload failed")
+                    self.mQueue.put(filePath)
+                else:
+                    logging.info("Upload success")
+                    #Write uploaded file to metafile
+                    f = open(metaFilePath, 'a')
+                    f.write(filePath)
+                    f.close()
             except Queue.Empty:
                 continue
+
+    def setSaveLocation(self, path):
+        self.defaultPath = path
 
     def join(self, timeout=None):
         self.stoprequest.set()
@@ -51,6 +68,67 @@ class Upload(threading.Thread):
 
     def doResume(self):
         logging.info('Resume upload file ')
+
+    def getCredentials(self):
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.getcwd()
+        credential_path = os.path.join(home_dir,
+                                       'credential.json')
+
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            flags.noauth_local_webserver = True
+            credentials = tools.run_flow(flow, store, flags)
+            logging.info('Storing credentials to ' + credential_path)
+
+        http = credentials.authorize(httplib2.Http())
+        self.service = discovery.build('drive', 'v2', http=http)
+
+    def insertFile(self, title, description, parent_id, mime_type, filename):
+        """Insert new file.
+
+        Args:
+            service: Drive API service instance.
+            title: Title of the file to insert, including the extension.
+            description: Description of the file to insert.
+            parent_id: Parent folder's ID.
+            mime_type: MIME type of the file to insert.
+            filename: Filename of the file to insert.
+          Returns:
+            Inserted file metadata if successful, None otherwise.
+        """
+        media_body = MediaFileUpload(filename, mimetype=mime_type, resumable=True)
+        body = {
+          'title': title,
+          'description': description,
+          'mimeType': mime_type
+        }
+        # Set the parent folder.
+        if parent_id:
+            body['parents'] = [{'id': parent_id}]
+
+        try:
+            file = self.service.files().insert(
+                body=body,
+                media_body=media_body).execute()
+
+            # Uncomment the following line to print the File ID
+            # print 'File ID: %s' % file['id']
+
+            return file
+        except errors.HttpError as error:
+            logging.info ('An error occurred: %s' % error)
+            return None
 
 
 
